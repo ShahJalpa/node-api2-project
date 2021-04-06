fb4362ab098c9f9dcad86d39d8577266b4407c6d
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-06 16:49:03 -0400

    server has started

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const Posts = require("./posts-model.js");
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Posts.find()
+         .then(posts => {
+             res.status(200).json(posts);
+         })
+         .catch(error => {
+             res.status(500).json({
+                 message: 'Error retriving the posts',
+             })
+         })
+})
+module.exports = router
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require("./api/server")
+
+const port = require(5000)
+
+server.listen(port, () => {
+    console.log("server listening on port 5000")
+})
