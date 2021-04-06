f53fbd90bb2e4d4d326d3ece227664d99fe10b01
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-06 19:26:31 -0400

    put request is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -53,4 +53,31 @@ router.post('/', (req, res) => {
              })
     }
 })
+
+router.put('/:id', (req, res) =>{
+    const {id} =req.params
+    const updatePost = req.body
+
+    if(!updatePost.title || !updatePost.contents){
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    }else{
+        Posts.update(id, updatePost)
+             .then(post =>{
+                 if(!post){
+                     res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                     })
+                 }else{
+                     res.status(200).json(post)
+                 }
+             })
+             .catch(error => {
+                 res.status(500).json({
+                    message: "The post information could not be modified"
+                 })
+             })
+    }
+})
 module.exports = router
