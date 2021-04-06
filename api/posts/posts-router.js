f6c11ea3f0c35db1c9f919d334e62a64fdfb6b32
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-06 19:13:39 -0400

    post request is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -33,4 +33,24 @@ router.get('/:id', (req, res) =>{
              })
          })
 })
+
+router.post('/', (req, res) => {
+    const newPost = req.body
+    console.log(newPost.title, newPost.contents)
+    if(!newPost.title || !newPost.contents){
+        res.status(400).json({
+            message: "Please provide title and contents for the post" 
+        })
+    }else{
+        Posts.insert(newPost)
+             .then(post => {
+                 res.status(201).json(post)
+             })
+             .catch(error => {
+                 res.status(500).json({
+                    message: "There was an error while saving the post to the database"
+                 })
+             })
+    }
+})
 module.exports = router
