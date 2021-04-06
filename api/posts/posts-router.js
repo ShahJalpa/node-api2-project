1441aa99980087dabb559984cf2d010fd571dfae
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-06 19:00:23 -0400

    find all and findById are responding as expected

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,7 +10,26 @@ router.get('/', (req, res) => {
          })
          .catch(error => {
              res.status(500).json({
-                 message: 'Error retriving the posts',
+                message: "The posts information could not be retrieved",
+             })
+         })
+})
+
+router.get('/:id', (req, res) =>{
+    Posts.findById(req.params.id)
+         .then(post => {
+            if(post){
+                res.status(200).json(post)
+            }else{
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            }
+         })
+         .catch(error => {
+             console.log(error)
+             res.status(500).json({
+                message: "The post information could not be retrieved"
              })
          })
 })
