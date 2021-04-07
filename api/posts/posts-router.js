691869df01a25ff0e34d370edf6e7f77e1ebd8a8
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-06 20:05:16 -0400

    get id/comments working and MVP is done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -101,4 +101,22 @@ router.delete('/:id', async(req, res) => {
     }
   
 })
+
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+         .then(comments =>{
+             if(comments){
+                 res.status(200).json(comments)
+             }else{
+                 res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                 })
+             }
+         })
+         .catch(error => {
+             res.status(500).json({
+                message: "The comments information could not be retrieved"
+             })
+         })
+})
 module.exports = router
